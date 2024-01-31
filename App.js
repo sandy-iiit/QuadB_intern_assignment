2ed0dbb82458058da97fa0f1ca1292b4b2b56627
Sandy <95034962+sandy-iiit@users.noreply.github.com>
2024-02-01 03:02:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, createBrowserRouter, Route, RouterProvider, Switch} from 'react-router-dom';
 import ShowList from './components/ShowList';
 import ShowDetails from './components/ShowDetails';
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={ShowList} />
-                <Route path="/show/:id" component={ShowDetails} />
-            </Switch>
-        </Router>
-    );
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<ShowList/>
+    },
+    {
+      path:"/show/:id",
+      element:<ShowDetails/>
+    }
+  ])
+  return <RouterProvider router={router} />;
+
+
 }
 
 export default App;
